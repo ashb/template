402ef61a95f4b7f8fba9da683ca8f23e2a4b05d0
Ash Berlin <ash_github@firemirror.com>
2008-12-14 13:18:04 +0000

    Add more of the test suite from TT. This fails right now

---
+++ t/binop.js
@@ -0,0 +1,32 @@
+$importer.context = this;
+
+$importer.paths.unshift('t/lib', 'lib');
+
+$importer.load('Template');
+$importer.load('TestHarness');
+$importer.load('Template.Test');
+
+warn = Function.bind(IO.stderr, 'print');
+
+t = new Template.Test();
+t.name = 'vars';
+
+var counter = 0;
+t.build_tests(new IO.File('t/binop.data'),
+              new Template({INTERPOLATE: 1, POST_CHOMP: 1}),
+              { yes: 1,
+                no: 0,
+                'true': 'this is true',
+                'false': '0',
+                happy: 'yes',
+                sad: '',
+                ten: 10,
+                twenty: 20,
+                alpha: function() { return ++counter; },
+                omega: function() { counter += 10; return 0; },
+                count: function() { return couter; },
+                reset: function() { return counter = 0; }
+              });
+
+//TestHarness.go();
+t.run('test_1');
